0e951f5706407cdd138cd78757fa86edd68b9242
e5000449 <e5000449@192.168.43.206>
2021-01-23 13:27:25 +0530

    2nd commit

---
+++ maveensample/src/test/java/maveensample/test.java
@@ -0,0 +1,10 @@
+package maveensample;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("emulator is opened");
+	}
+
+}
